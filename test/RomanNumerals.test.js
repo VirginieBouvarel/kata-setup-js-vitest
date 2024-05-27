1b2321ceb5337e2438c6fc3ce996915d29ea605c
Virginie <vb@ikimo9.com>
2024-05-27 09:26:15 +0200

    starting point

---
+++ test/RomanNumerals.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { convert } from '../src/RomanNumerals';
+
+describe('Roman Numerals', () => {
+  it('should return true', () => {
+    expect(convert()).toBe(true);
+  })
+});
+
