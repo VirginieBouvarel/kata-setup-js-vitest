cf7f58cdc0e3a769cc4c56e906812240cc3e3607
Virginie <vb@ikimo9.com>
2024-05-27 09:21:53 +0200

    init

---
+++ test/Kata.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { kata } from '../src/Kata';
+
+describe('Kata Title', () => {
+  it('should return true', () => {
+    expect(kata()).toBe(true);
+  })
+});
+
