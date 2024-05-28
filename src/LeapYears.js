c8c39988b94b1631b28859a5b076b9887ebf4073
Virginie <vb@ikimo9.com>
2024-05-28 14:25:42 +0200

    wip

--- src/LeapYears.js
+++ src/LeapYears.js
@@ -1,4 +1,17 @@
 
-export const isLeapYears =  () => {
-  return false;
+/**
+ * 
+ * Une année est bisextile si :
+ * elle est divisible par 400 -> ex : 2000
+ * elle est divisible par 4 MAIS PAS par 1000 -> ex : 2008, 2012, 2016
+ * une année n'est pas bisextile si :
+ * elle n'est pas divisible par 4 -> 2017, 2018, 2019
+ * elle est divisible par 100 mais pas par 400 -> 1700, 1800, 1900, 2100
+ */
+
+export const isLeapYears =  (year) => {
+  if (year === 1800) return false;
+  if (year === 1700) return false;
+  if (year === 2008) return true;
+  if (year === 2000) return true;
 };
--- test/LeapYears.test.js
+++ test/LeapYears.test.js
@@ -2,8 +2,37 @@ import { describe, it, expect } from 'vitest';
 import { isLeapYears } from '../src/LeapYears';
 
 describe('LeapYears', () => {
-  it('should return true', () => {
-    expect(isLeapYears()).toBe(true);
-  })
+  it('should return true for 2000', () => {
+    expect(isLeapYears(2000)).toBe(true);
+  });
+  /**
+   * Pour éviter d'avoir un test qui passe automatiquement
+   * à la prochaine étape
+   * on teste une année non bisextile
+   */
+  it('should return false for 1800', () => {
+    expect(isLeapYears(1800)).toBe(false);
+  });
+  it('should return false for 1700', () => {
+    expect(isLeapYears(1700)).toBe(false);
+  });
+  /**
+   * Remarque :
+   * On voit que les années bisextiles (2000) 
+   * comme les années non bisextiles (1800 et 1700)
+   * peuvent être divisible par 1000
+   * ce n'est pas un critère différenciant
+   */
+  it('should return false for 2008', () => {
+    expect(isLeapYears(2008)).toBe(true);
+  });
+/**
+ * Refacto :
+ * Pour faire apparaître le cas 2000 dans les vérifications
+ * Afin de faire apraître des similitudes
+ * return true ->   if (year === 2000) return true;
+ */
+
+
 });
 
