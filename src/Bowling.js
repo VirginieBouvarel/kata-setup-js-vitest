0ae3af11125336b38377e44a5652c9fe46808117
Virginie <vb@ikimo9.com>
2024-06-01 11:57:31 +0200

    solution consignes coding-dojo

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,13 +1,49 @@
-
 export class Game {
   constructor() {
-    this.score = 0;
+    this.rolls = [];
   }
-  getScore() {
-    return this.score;
+
+  roll(pins) {
+    this.rolls.push(pins);
   }
-  playARound(array) {
-    this.score += array[0];
-    this.score += array[1];
+  score() {
+    let rollsCopy = [...this.rolls];
+
+    let totalScore = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      totalScore += frameScore(rollsCopy);
+      rollsCopy = rollsCopy.slice(rollsToRemove(rollsCopy));
+    }
+
+    return totalScore;
   }
 }
+
+function frameScore(rollsCopy) {
+  if (isStrike(rollsCopy)) return strikeScore(rollsCopy);
+  if (isSpare(rollsCopy)) return spareScore(rollsCopy);
+  return normalScore(rollsCopy);
+}
+function rollsToRemove(rollsCopy) {
+  return isStrike(rollsCopy) ? 1 : 2;
+}
+function isStrike(rollsCopy) {
+  return rollsCopy[0] === 10;
+}
+function isSpare(rollsCopy) {
+  return rollsCopy[0] + rollsCopy[1] === 10;
+}
+function strikeScore(rollsCopy) {
+  return 10 + rollsCopy[1] + rollsCopy[2];
+}
+function spareScore(rollsCopy) {
+  return 10 + rollsCopy[2];
+}
+function normalScore(rollsCopy) {
+  return rollsCopy[0] + rollsCopy[1];
+}
+
+
+
+
+
--- test/Bowling.test.js
+++ test/Bowling.test.js
@@ -1,68 +1,73 @@
 /**
- * * Règles :
- * 10 manches -> 2 lancers par manche pour chaque joueur
- * 
- * * Objectif : 
- * faire tomber les 10 quilles en deux lancers
- * 
- * * Score d'une manche : 
- * nombre total de quilles renversées sur les 2 lancers 
- * + Bonus pour les spares et les strikes
- *
- * * SPARE : 
- * Les 10 quilles tombent en 2 lancers
- * score du spare : 10 + Bonus -> nombre de quilles du lancer suivant
- *
- * * STRIKE :
- * Les 10 quilles tombent au 1er lancer
- * Il n'y a pas de second lancer
- * score du strike : 10 + Bonus -> nombre de quilles des 2 lancers suivants
- *
- * * MANCHE 10 :
- * Si SPARE à la dernière manche 
- * -> 1 lancer de plus autorisé pour calculer le bonus (2 + 1 = 3 lancers en tout)
- * Si STRIKE à la dernière manche 
- * -> 2 lancers de plus autorisés pour calculer le bonus (1 + 2 = 3 lancers en tout)
- * 
- * * Exigences :
- * Ecrire une classe Game
- * Qui a deux méthodes : 
- * * * void roll(int) 
- * -> appelée à chaque fois que le joueur fait rouler une balle, 
- * -> int est le nombre de quilles renversées
- * * * int score()
- * -> renvoie le score total du jeu
- * 
- * * Article
- * Lisez cet article « Engineer Notebook : An Extreme Programming Episode » de Robert C. Martin, 
- * où il décrit la résolution de ce kata avec Robert S. Koss. 
- * butunclebob.com/files/downloads/Bowling%20Game%20Kata.ppt
- */
+* Règles :
+* 1 game = 10 frames
+* failed score = number of pins
+* spare = 10 + number of pins in the next roll
+* strike = 10 + number of pins in the next two rolls
+* spare in the last frame  = one more bonus roll
+* strike in the last frame = two more bonus rolls
+* spare and strike more bonus rolls are only used to calculate bonus
+* total score is the total of all frame scores
+
+* Objectif : calculate the total score
+* for a valid sequence of rolls
+*/
+
+// RED
 
 import { describe, expect, it } from 'vitest';
 import { Game } from '../src/Bowling';
 
+
 describe('Bowling', () => {
-  it('should score 0 before the start of the game', () => {
+  it('should score 6 for a [11220*16] game', () => {
     const game = new Game;
-    expect(game.getScore()).toBe(0);
+    game.roll(1);
+    game.roll(1);
+    game.roll(2);
+    game.roll(2);
+    for (let i = 0; i < 16; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(6);
   });
-  it('should score 9 for a round with 8 pins knocked down at the first roll and 1 at the second', () => {
+  it('should score 12 for a game with a spare', () => {
     const game = new Game;
-    game.playARound([8,1]);
-    expect(game.getScore()).toBe(9);
+    game.roll(5);
+    game.roll(5);
+    game.roll(1);
+    for (let i = 0; i < 17; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(12);
   });
-  it('should score 7 for a round with 0 pin knocked down at the first roll and 7 at the second', () => {
+  it('should score 21 for a [545510*15] game', () => {
     const game = new Game;
-    game.playARound([0,7]);
-    expect(game.getScore()).toBe(7);
+    game.roll(5);
+    game.roll(4);
+    game.roll(5);
+    game.roll(5);
+    game.roll(1);
+    for (let i = 0; i < 15; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(21);
   });
-  it('should score 16 for a round with 0 pin knocked down at the first roll and 7 at the second after a round with 8 pins knocked down at the first roll and 1 at the second', () => {
+  it('should score 12 for a game with a strike', () => {
     const game = new Game;
-    game.playARound([8,1]);
-    game.playARound([0,7]);
-    expect(game.getScore()).toBe(16);
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    for (let i = 0; i < 17; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(14);
+  });
+  it('should score 300 for a perfect game', () => {
+    const game = new Game;
+    for (let i = 0; i < 12; i++) {
+      game.roll(10);
+    }
+    expect(game.score()).toBe(300);
   });
 });
-
-
