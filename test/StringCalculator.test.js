7e52f30ef792369eaecdd48a13cd2b7c86580136
Virginie <vb@ikimo9.com>
2024-06-03 20:58:36 +0200

    init test runner

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,9 +1,8 @@
 import { describe, it, expect } from 'vitest';
-import { kata } from '../src/Kata';
 
 describe('Kata Title', () => {
   it('should return true', () => {
-    expect(kata()).toBe(true);
+    expect(true).toBe(true);
   })
 });
 
