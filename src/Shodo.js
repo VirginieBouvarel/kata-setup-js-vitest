cee2bf01339b74d7c7a30bb96eb742b528931ee0
Virginie <vb@ikimo9.com>
2024-06-05 15:11:27 +0200

    wip-entretien-tech-ludo-lea

--- src/Shodo.js
+++ src/Shodo.js
@@ -1,1 +1,12 @@
-
+export class Wallet {
+ constructor(assets) {
+  this.assets = assets;
+  this.conversionsToEuro = { euro : 1, dollar: 2 };
+ }
+  amount() {
+    if (!this.assets) return 0;
+    return this.assets.reduce((sum, currentValue) => {
+      return sum += currentValue.quantity * this.conversionsToEuro[currentValue.type];
+    }, 0);
+  }
+}
--- test/Shodo.test.js
+++ test/Shodo.test.js
@@ -1,8 +1,31 @@
-import { describe, expect, it } from 'vitest';
+/**
+ * Règles :
+ * Wallet
+ * Given a wallet containing financial assets, build a function that compute the value of wallet in * a currency.
+ * The financial assets have a quantity and a type which can be for example Apple, Euros, Bitcoins
+ * and Dollars.
+ * To value the wallet in a currency you could use an external api to provide rate exchanges
+ */
 
-describe('Kata Title', () => {
-  it('should return true', () => {
-    expect(true).toBe(false);
-  })
+
+import { describe, expect, it } from 'vitest';
+import { Wallet } from '../src/Shodo';
+
+
+describe('Wallet', () => {
+  it('should return 0 for an empty wallet', () => {
+    const wallet = new Wallet();
+    expect(wallet.amount()).toBe(0);
+  });
+  it('should return 2 for wallet with two euro assets', () => {
+    const assets = [ { type: 'euro', quantity: 1 }, { type: 'euro', quantity: 1 } ];
+    const wallet = new Wallet(assets);
+    expect(wallet.amount()).toBe(2);
+  });
+  it('should return 3 for wallet with one euro and one dollar assets', () => {
+    const assets = [ { type: 'euro', quantity: 1 }, { type: 'dollar', quantity: 1 } ];
+    const wallet = new Wallet(assets);
+    expect(wallet.amount()).toBe(3);
+  });
 });
 
