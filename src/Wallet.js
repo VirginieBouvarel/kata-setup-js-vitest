53c3777e644396c9c05ee2081cb0795d16b0a989
Virginie <vb@ikimo9.com>
2024-06-06 15:34:27 +0200

    solution

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,1 +1,18 @@
+export class Wallet {
+  assets = [];
+  conversionTo = {
+    euros: { euro: 1, dollar: 2, bitcoin: 10},
+    dollars: { euro: 0.5, dollar: 1, bitcoin: 5 },
+    bitcoins: { euro: 0.1, dollar: 0.2, bitcoin: 1 },
+  };
 
+  addAsset(asset) {
+    this.assets.push(asset);
+  }
+  amount(currency = 'euros') {
+    return this.assets.reduce((sum, asset) => sum += this.valueIn(currency, asset), 0);
+  }
+  valueIn(currency, asset) {
+    return asset.quantity * this.conversionTo[currency][asset.type];
+  }
+}
--- test/Wallet.test.js
+++ test/Wallet.test.js
@@ -1,8 +1,75 @@
+/**
+ * Règles :
+ * Given a wallet containing financial assets, 
+ * build a function that compute the value of wallet in a currency.
+ * The financial assets have a quantity and a type 
+ * which can be for example Apple, Euros, Bitcoins and Dollars.
+ * To value the wallet in a currency you could use an external api to provide rate exchanges
+ */
+
+ // RED
+
 import { describe, expect, it } from 'vitest';
+import { Wallet } from '../src/Wallet';
 
 describe('Wallet', () => {
-  it('should return true', () => {
-    expect(true).toBe(true);
-  })
+  it('should return 0 for an empty wallet', () => {
+    const wallet = new Wallet();
+    expect(wallet.amount()).toBe(0);
+  });
+  it('should return 1 for a wallet with one euro', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 1 });
+    expect(wallet.amount()).toBe(1);
+  });
+  it('should return 2 for a wallet with two assets of one euro', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 1 });
+    wallet.addAsset({ type: 'euro', quantity: 1 });
+    expect(wallet.amount()).toBe(2);
+  });
+  it('should return 3 for a wallet with two assets of one and two euros', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 1 });
+    wallet.addAsset({ type: 'euro', quantity: 2 });
+    expect(wallet.amount()).toBe(3);
+  });
+  it('should return 2 for a wallet with one dollar', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'dollar', quantity: 1 });
+    expect(wallet.amount()).toBe(2);
+  });
+  it('should return 10 for a wallet with one bitcoin', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'bitcoin', quantity: 1 });
+    expect(wallet.amount()).toBe(10);
+  });
+  it('should return 25 for a wallet with one euro, 2 dollars and 2 bitcoins', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 1 });
+    wallet.addAsset({ type: 'dollar', quantity: 2 });
+    wallet.addAsset({ type: 'bitcoin', quantity: 2 });
+    expect(wallet.amount()).toBe(25);
+  });
+  it('should return 1 for a wallet with two euros, in dollars', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 2 });
+    expect(wallet.amount('dollars')).toBe(1);
+  });
+  it('should return 2 for a wallet with four euros, in dollars', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 4 });
+    expect(wallet.amount('dollars')).toBe(2);
+  });
+  it('should return 1 for a wallet with ten euros, in bitcoins', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 10 });
+    expect(wallet.amount('bitcoins')).toBe(1);
+  });
+  it('should return 2 for a wallet with twenty euros, in bitcoins', () => {
+    const wallet = new Wallet();
+    wallet.addAsset({ type: 'euro', quantity: 20 });
+    expect(wallet.amount('bitcoins')).toBe(2);
+  });
 });
 
